e96c689550f4e0f492e78fc9c62c045279d526e1
minhnguyenwp <minhnguyenwp@gmail.com>
2016-03-23 00:52:51 +0800

    M - update

--- html_tpl/source/assets/js/actions/detail.js
+++ html_tpl/source/assets/js/actions/detail.js
@@ -3,6 +3,8 @@
  * 1. MAPs
  * 2. Grid layout
  * 3. Event click maps
+ * 4. Tabs Terms
+ * 5. gallery box
  */
 var detail_fn = {};
 (function( $ ) {
@@ -26,6 +28,7 @@ detail_fn.MAPs = {
 		  zoom: 15,
 		  center: myLatlng,
 		  scrollwheel: false,
+		  mapTypeControl: false,
 		};
 
 		var map = new google.maps.Map(document.getElementById("detail-map"), mapOptions);
@@ -44,7 +47,6 @@ detail_fn.MAPs = {
 	 * @return {[type]} [description]
 	 */
 	streetView : function (data_lat, data_lng) {
-		console.log(1);
 		var data_lat = $('#street-map').data('lat'),
 			data_lng = $('#street-map').data('lng');
 
@@ -94,6 +96,13 @@ detail_fn.evtClickMap = function (){
 				// invoke street view
 				detail_fn.MAPs.streetView();
 			}
+		} else if($(this).hasClass('view-gallery')) {
+			if(!$(this).hasClass('active')) {
+				$(this).addClass('active');
+				$(this).siblings('a').removeClass('active');
+
+				$('.gl-item').click();
+			}
 		} else {
 			if(!$(this).hasClass('active')) {
 				$(this).addClass('active');
@@ -107,18 +116,143 @@ detail_fn.evtClickMap = function (){
 		}
 	});
 };
+/**
+ * 4. Tabs Terms
+ */
+detail_fn.tabsTerm = {
+	/**
+	 * [clickTabLongTerm description]
+	 * @return {[type]} [description]
+	 */
+	clickTabLongTerm : function () {
+		if(!$('.term-wrap .nav li').length) { return; }
+
+		$('.term-wrap .nav li').on('click', function (e) {
+			if($(this).hasClass('long-term')) {
+				$(this).closest('.detail-map').addClass('h-580');
+			} else {
+				$(this).closest('.detail-map').removeClass('h-580');
+			}
+		});
+	},
+	/**
+	 * [showSummary description]
+	 * @return {[type]} [description]
+	 */
+	showSummary : function () {
+		if(!$('.book-now').length) { return; }
+
+		$('.book-now').on('click', function (e) {
+			var $a_book		=	$(this),
+				$blk_price	=	$a_book.closest('.get-price-form'),
+				$blk_sum	=	$blk_price.siblings('.summary-form'),
+				$blk_map	=	$a_book.closest('.detail-map'),
+				$a_back		=	$blk_sum.find('.back-state');
+			if(!$a_book.hasClass('active')) {
+				$a_book.addClass('active');
+				$blk_price.hide();
+				$blk_sum.show();
+				$blk_map.addClass('h-720');
+				$a_back.removeClass('active');
+			}
+		});
+	},
+	/**
+	 * [closeSummary description]
+	 * @return {[type]} [description]
+	 */
+	closeSummary : function () {
+		if(!$('.back-state').length) { return; }
+
+		$('.back-state').on('click', function (e) {
+			var $a_back		=	$(this),
+				$blk_sum	=	$a_back.closest('.summary-form'),
+				$blk_price	=	$blk_sum.siblings('.get-price-form'),
+				$blk_map	=	$a_back.closest('.detail-map'),
+				$a_book		=	$blk_price.find('.book-now');
+			if(!$a_back.hasClass('active')) {
+				$a_back.addClass('active');
+				$blk_price.show();
+				$blk_sum.hide();
+				$blk_map.removeClass('h-720');
+				$a_book.removeClass('active');
+			}
+		});
+	},
+	/**
+	 * [showTermMB description]
+	 * @return {[type]} [description]
+	 */
+	showTermMB : function () {
+		if(!$('.mb_term').length) { return; }
+
+    	$('.mb_term').on('click', function (e) {
+    		var $a_term		=	$(this),
+    			$body		=	$a_term.closest('body')
+    			$term_wrap	=	$a_term.siblings('.blk-frm-term');
+
+    		$term_wrap.addClass('shw-term-mb');
+			$body.addClass('bd-fixed');
+    	});
+	},
+
+	/**
+	 * [closeTermMB description]
+	 * @return {[type]} [description]
+	 */
+	closeTermMB : function () {
+		if(!$('.mb-close-term').length) { return; }
+
+    	$('.mb-close-term').on('click', function (e) {
+    		var $a_term		=	$(this),
+    			$body		=	$a_term.closest('body')
+    			$term_wrap	=	$a_term.closest('.blk-frm-term');
+
+    		$term_wrap.removeClass('shw-term-mb');
+			$body.removeClass('bd-fixed');
+    	});
+	}
+};
+/**
+ * 5. gallery box
+ */
+detail_fn.galleryBox = function () {
+	if(!$('.gal-box').length) { return; }
+
+	$('.gal-box').lightGallery({
+		selector: '.gl-item',
+		loop: true,
+        fourceAutoply: false,
+        autoplay: false,
+        thumbnail: false,
+        pager: $(window).width() >= 768 ? true : false,
+        speed: 300,
+        scale: 1,
+        keypress: true,
+        download: false
+	}); 
+};
 /* ----------------------------------------------- */
 /* ----------------------------------------------- */
 /* OnLoad Page */
 $(document).ready(function($){
 	// MAPs
 	detail_fn.MAPs.init();
-
 	// grid layout
 	detail_fn.gridLayout ();
 
 	// event click maps
 	detail_fn.evtClickMap();
+
+	// tabs
+	detail_fn.tabsTerm.clickTabLongTerm ();
+	detail_fn.tabsTerm.showSummary();
+	detail_fn.tabsTerm.closeSummary();
+	detail_fn.tabsTerm.showTermMB();
+	detail_fn.tabsTerm.closeTermMB();
+
+	// gallery
+	detail_fn.galleryBox();
 });
 /* OnLoad Window */
 var init = function () {   
